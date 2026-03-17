4c4aff420fd113803ab28063a5006cdbabd08dd1
conao3 <conao3@gmail.com>
2026-03-17 19:41:51 +0900

    fmt

--- dev/user.clj
+++ dev/user.clj
@@ -1,20 +1,26 @@
 (ns user
   (:require
-   [blog-clojure.core :as blog-clojure]
-   [clojure.java.io :as io]
-   [clojure.data.json :as json]
-   [babashka.fs :as fs]))
+    [babashka.fs :as fs]
+    [blog-clojure.core :as blog-clojure]
+    [clojure.data.json :as json]
+    [clojure.java.io :as io]))
+
 
 (defonce server (atom nil))
 
-(defn go []
+
+(defn go
+  []
   (when @server
     (.stop @server))
   (->> (blog-clojure/start-server)
        (reset! server)))
 
-(defn generate-css-1 [file]
-  (letfn [(getfn [m]
+
+(defn generate-css-1
+  [file]
+  (letfn [(getfn
+            [m]
             (let [schema (:$schema m)
                   known-value-types #{"https://opensource.adobe.com/spectrum-tokens/schemas/token-types/alias.json"
                                       "https://opensource.adobe.com/spectrum-tokens/schemas/token-types/color.json"
@@ -44,23 +50,27 @@
 
                 :else
                 (throw (ex-info "unknown schema" {:schema schema})))))
-          (format-val [v]
+          (format-val
+            [v]
             (if-let [m (and (string? v)
                             (re-matches #"\{(.*)\}" v))]
               (format "var(--%s)" (second m))
               (cond->> v (double? v) (format "%.2f"))))]
     (->
-     (json/read-str (slurp (io/resource (format "spectrum/%s.json" file))) :key-fn keyword)
-     (update-vals getfn)
-     (->> (sort-by (fn [[k _]] (if-let [m (re-matches #"^(.*)-([0-9]+)$" (name k))]
-                                 (str (nth m 1) (format "%05d" (parse-long (nth m 2))))
-                                 (name k))))
-          (map (fn [[k v]] (format "--%s: %s;\n" (name k) (format-val v))))
-          (apply str)
-          (format ":root {\n%s}\n")
-          (spit (format "generated/spectrum/%s.css" file))))))
+      (json/read-str (slurp (io/resource (format "spectrum/%s.json" file))) :key-fn keyword)
+      (update-vals getfn)
+      (->> (sort-by (fn [[k _]]
+                      (if-let [m (re-matches #"^(.*)-([0-9]+)$" (name k))]
+                        (str (nth m 1) (format "%05d" (parse-long (nth m 2))))
+                        (name k))))
+           (map (fn [[k v]] (format "--%s: %s;\n" (name k) (format-val v))))
+           (apply str)
+           (format ":root {\n%s}\n")
+           (spit (format "generated/spectrum/%s.css" file))))))
 
-(defn generate-css []
+
+(defn generate-css
+  []
   (doseq [file ["color-aliases"
                 "color-component"
                 "color-palette"
--- src/blog_clojure/core.clj
+++ src/blog_clojure/core.clj
@@ -1,16 +1,19 @@
 (ns blog-clojure.core
   (:require
-   [clojure.java.io :as io]
-   [markdown.core :as markdown]
-   [hiccup.page :as hiccup.page]
-   [hiccup2.core :as hiccup]
-   [stasis.core :as stasis]
-   [ring.adapter.jetty :as jetty]))
+    [clojure.java.io :as io]
+    [hiccup.page :as hiccup.page]
+    [hiccup2.core :as hiccup]
+    [markdown.core :as markdown]
+    [ring.adapter.jetty :as jetty]
+    [stasis.core :as stasis]))
+
 
 (def blog-name "Conao3 Notes")
 (def stasis-config {:stasis/ignore-nil-pages? true})
 
-(defn slurp-binary-directory [dir re]
+
+(defn slurp-binary-directory
+  [dir re]
   (->> (file-seq (io/file dir))
        (filter (fn [file] (and (.isFile file) (re-find re (.getName file)))))
        (map (fn [file]
@@ -22,31 +25,37 @@
                           (.toByteArray out)))])))
        (into {})))
 
-(defn render-page [{:keys [title body]}]
+
+(defn render-page
+  [{:keys [title body]}]
   (hiccup.page/html5 {:lang "ja"}
-   [:head
-    [:meta {:http-equiv "Content-Type" :content "text/html; charset=utf-8"}]
-    [:meta {:name "viewport" :content "width=device-width,initial-scale=1"}]
-    [:title (format "%s - %s" title blog-name)]
-    (hiccup.page/include-css "/assets/modern-css-reset.css")
-    (hiccup.page/include-css "/assets/spectrum/color-palette.css")
-    (hiccup.page/include-css "/assets/spectrum/typography.css")
-    (hiccup.page/include-css "/assets/index.css")]
-   [:body
-    [:h2 blog-name]
-    [:div {:style {:display "flex"}}
-     [:a {:href "/"} "Home"]]
-    (hiccup/raw body)]))
+                     [:head
+                      [:meta {:http-equiv "Content-Type" :content "text/html; charset=utf-8"}]
+                      [:meta {:name "viewport" :content "width=device-width,initial-scale=1"}]
+                      [:title (format "%s - %s" title blog-name)]
+                      (hiccup.page/include-css "/assets/modern-css-reset.css")
+                      (hiccup.page/include-css "/assets/spectrum/color-palette.css")
+                      (hiccup.page/include-css "/assets/spectrum/typography.css")
+                      (hiccup.page/include-css "/assets/index.css")]
+                     [:body
+                      [:h2 blog-name]
+                      [:div {:style {:display "flex"}}
+                       [:a {:href "/"} "Home"]]
+                      (hiccup/raw body)]))
 
-(defn render-content [{:keys [title body date]}]
+
+(defn render-content
+  [{:keys [title body date]}]
   (hiccup/html
-   [:article
-    [:h1 title]
-    [:div date]
-    [:hr]
-    (hiccup/raw body)]))
+    [:article
+     [:h1 title]
+     [:div date]
+     [:hr]
+     (hiccup/raw body)]))
 
-(defn site []
+
+(defn site
+  []
   (let [contents (->> (stasis/slurp-directory "generated/contents" #"\.md$")
                       (map (fn [[k v]]
                              (let [p (markdown/md-to-html-string-with-meta v :heading-anchors true)
@@ -57,24 +66,26 @@
                                 (assoc obj :body (render-content obj))])))
                       (into {}))
         blog-inx (hiccup/html
-                  [:div
-                   [:h1 "Blogs"]
-                   [:ul
-                    (->> contents
-                         (map (fn [[k v]] [:li [:a {:href k} (:title v)]]))
-                         (sort-by (comp :date second))
-                         reverse)]])
+                   [:div
+                    [:h1 "Blogs"]
+                    [:ul
+                     (->> contents
+                          (map (fn [[k v]] [:li [:a {:href k} (:title v)]]))
+                          (sort-by (comp :date second))
+                          reverse)]])
         contents (update-in contents ["/index.html" :body]
                             #(str % blog-inx))]
     (stasis/merge-page-sources
-     {:contents (-> contents
-                    (update-vals render-page)
-                    (->> (into {})))
-      :public (slurp-binary-directory "resources/public" #"\.[^.]+$")
-      :spectrum (-> (stasis/slurp-directory "generated/spectrum" #"\.[^.]+$")
-                    (update-keys (partial str "/assets/spectrum")))})))
+      {:contents (-> contents
+                     (update-vals render-page)
+                     (->> (into {})))
+       :public (slurp-binary-directory "resources/public" #"\.[^.]+$")
+       :spectrum (-> (stasis/slurp-directory "generated/spectrum" #"\.[^.]+$")
+                     (update-keys (partial str "/assets/spectrum")))})))
 
-(defn export [& _args]
+
+(defn export
+  [& _args]
   (let [export-dir "./target"
         load-export-dir #(stasis/slurp-directory export-dir #"\.[^.]+$")
         old-files (load-export-dir)]
@@ -84,7 +95,9 @@
     (println "Export complete:")
     (stasis/report-differences old-files (load-export-dir))))
 
-(defn start-server [& _args]
+
+(defn start-server
+  [& _args]
   (jetty/run-jetty
     (stasis/serve-pages site stasis-config)
     {:port 8080 :join? false}))
